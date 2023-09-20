d1fb80211aa9cca684f936f1fbe1a059e68a52ef
ViktorKozh <viktor.ru253@gmail.com>
2023-09-20 19:05:16 +0300

    Commit 6

--- PyLab2.py
+++ PyLab2.py
@@ -2,11 +2,11 @@ import timeit
 
 def f():
     j = 0
-    for i in range(10000):
+    for i in range(100000):
         j += 1
     print("end")
 
 for i in range(100):
     print("Something number", i)
 
-print("Python can count to 11000 in", timeit.timeit(lambda: f(), number=1))
+print("Python can count to 101000 in", timeit.timeit(lambda: f(), number=1))
